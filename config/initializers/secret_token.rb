63b1019a5e9fdc5ecede3bdebe2a61803aa0bfa8
Yordan <yordan@wego.com>
2014-04-24 14:09:26 +0700

    getting started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facturas::Application.config.secret_key_base = 'c1931640ae613f9f5ed6d68bbd1f19386b78b3626540fde241b8308df1728fdfba2ea4eb9a8627aef8dc6b95789128e7b11cef74ba63abba10a6064750b7ba8e'
